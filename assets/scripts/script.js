6fb9d9d69486d86849f2faca0c366a90c35d01f5
sean <seanyoung247@gmail.com>
2024-03-30 12:50:40 +0000

    feat: Adds submission dialog

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,7 +3,17 @@
 
     const section = document.querySelector('#camp-activities-inquiry');
     const activities = document.querySelector('#activity-select');
+    const loadingTemp = document.querySelector('#submit-template');
     activities.required = true;
+
+    const submitDialog = loadingTemp.content.firstElementChild.cloneNode(true);
+    document.body.append(submitDialog);
+
+    const resetBtn = document.querySelector('#reset-btn');
+    resetBtn.addEventListener('click', ()=>{
+        section.className = '';
+        submitDialog.className = '';
+    });
 
     activities.addEventListener('change', () => {
         if (activities.value !== '') activities.classList.remove('invalid');
@@ -19,6 +29,7 @@
     document.querySelector("#camp-activities-inquiry > form > button[type='submit']")
         .addEventListener('click', e => {
             e.preventDefault();
+
             if (activities.value === '') {
                 activities.classList.add('invalid');
                 // Forces the invalid animation to replay
@@ -29,7 +40,14 @@
             }
 
             section.classList.add('submitting');
+            submitDialog.classList.add('submitting');
+
+            // Simulating callback to server:
+            setTimeout(() => {
+                submitDialog.classList.remove('submitting');
+                submitDialog.classList.add('submitted');
+                resetBtn.disabled = false;
+            }, 5000);
         });
 
 })();
-      
